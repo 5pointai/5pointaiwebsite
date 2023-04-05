565aa9fd9625a685df6fcbd1b8e32fb3ce3b8f10
vikas94 <kesamreddyv@gmail.com>
2023-04-06 01:46:08 +0530

    changed form code

--- contact_form.php
+++ contact_form.php
@@ -12,38 +12,40 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Clean and validate input data
-function clean_input($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Clean and validate input data
+    function clean_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+    $name = clean_input($_POST['name']);
+    $email = clean_input($_POST['email']);
+    $phone = clean_input($_POST['phone']);
+    $enquiry = clean_input($_POST['enquiry']);
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        die("Invalid email format");
+    }
+
+    if (!preg_match("/^\+?[0-9]{10,15}$/", $phone)) { // Updated phone validation
+        die("Invalid phone number format");
+    }
+
+    // Prepare and bind
+    $stmt = $conn->prepare("INSERT INTO contact_form_data (name, email, phone, enquiry) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $name, $email, $phone, $enquiry);
+
+    if ($stmt->execute()) {
+        echo "<p class='success-message'>We'll get in touch with you shortly.</p>";
+    } else {
+        echo "<p class='error-message'>Error: " . $stmt->error . "</p>";
+    }
+
+    $stmt->close();
+    $conn->close();
 }
-
-$name = clean_input($_POST['name']);
-$email = clean_input($_POST['email']);
-$phone = clean_input($_POST['phone']);
-$enquiry = clean_input($_POST['enquiry']);
-
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    die("Invalid email format");
-}
-
-if (!preg_match("/^\+?[0-9]{10,15}$/", $phone)) { // Updated phone validation
-    die("Invalid phone number format");
-}
-
-// Prepare and bind
-$stmt = $conn->prepare("INSERT INTO contact_form_data (name, email, phone, enquiry) VALUES (?, ?, ?, ?)");
-$stmt->bind_param("ssss", $name, $email, $phone, $enquiry);
-
-if ($stmt->execute()) {
-    header("Location: thank_you.html"); // Redirect to a thank you page after successful submission
-} else {
-    echo "Error: " . $stmt->error;
-}
-
-$stmt->close();
-$conn->close();
 ?>

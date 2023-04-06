7bbd968dc0f0d1fc5c4beb71fdf842e26ee211a7
vikas94 <kesamreddyv@gmail.com>
2023-04-06 17:21:59 +0530

    rectified error in php

--- contact_form.php
+++ contact_form.php
@@ -1,18 +1,16 @@
 <?php
 require_once 'db_config.php';
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    error_log("Connection failed: " . $conn->connect_error);
+    die("We're experiencing technical difficulties. Please try again later.");
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
     // Clean and validate input data
     function clean_input($data)
     {
@@ -31,19 +29,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Invalid email format");
     }
 
-    if (!preg_match("/^\+?[0-9]{10,15}$/", $phone)) { // Updated phone validation
+    if (!preg_match("/^\+?[0-9]{10,15}$/", $phone)) {
         die("Invalid phone number format");
     }
 
     // Prepare and bind
     $stmt = $conn->prepare("INSERT INTO contact_form_data (name, email, phone, enquiry) VALUES (?, ?, ?, ?)");
+    if ($stmt === false) {
+        error_log("Prepare failed: " . $conn->error);
+        die("We're experiencing technical difficulties. Please try again later.");
+    }
     $stmt->bind_param("ssss", $name, $email, $phone, $enquiry);
 
     if ($stmt->execute()) {
+        // Redirect to thank_you.html after successful submission
         header("Location: thank_you.html");
         exit();
     } else {
-        echo "<p class='error-message'>Error: " . $stmt->error . "</p>";
+        error_log("Error: " . $stmt->error);
+        echo "<p class='error-message'>We're experiencing technical difficulties. Please try again later.</p>";
     }
 
     $stmt->close();
